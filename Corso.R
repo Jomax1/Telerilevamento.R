f7ec52ec368459caa0d3aad74584bc585160a263
Jomax1 <maxime1.joly@gmail.com>
2025-03-06 14:13:32 +0100

    Update Corso.R

--- Corso.R
+++ Corso.R
@@ -1,1 +1,19 @@
+#04/03/2025
+b2=im.import("sentinel.dolomites.b2.tif")
+cl = colorRampPalette(c("black","darkgrey","lightgrey"))(100)
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("greenyellow","cyan4","deeppink"))(100)
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("greenyellow","cyan4","deeppink"))(3)
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("cornflowerblue","firebrick","indianred1"))(100)
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("cornflowerblue","firebrick","khaki"))(100)
+plot(b2, col=cl)
+
+#06/03/2025
 
