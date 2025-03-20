864720bae1cd47b5b5af66daca11567e25562b76
Jomax1 <maxime1.joly@gmail.com>
2025-03-20 14:45:55 +0100

    Update Corso_250304.R

--- Code/Corso_250304.R
+++ Code/Corso_250304.R
@@ -6,6 +6,7 @@ install.packages("devtools")
 install.packages("viridis") #colori per daltonici
 install_github("ducciorocchini/imageRy")
 
+install.packages("ggridges") # this is needed to reate ridgeline plots
 
 library(terra)
 library(devtools)
