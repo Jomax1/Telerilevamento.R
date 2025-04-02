962b2b160199c7309761eea74ce38ffe3f841cae
Jomax1 <maxime1.joly@gmail.com>
2025-04-02 16:31:56 +0200

    Update 07_Classification.R

--- Code/07_Classification.R
+++ Code/07_Classification.R
@@ -8,9 +8,12 @@ mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
 mato1992 = flip(mato1992)
 plot(mato1992)
 
-
 mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
 mato2006 = flip(mato2006)
 plot(mato2006)
+
+#classificare
+mato1992c = im.classify(mato1992, num_clusters=2)
+mato2006c = im.classify(mato2006, num_clusters=2)
 
 
