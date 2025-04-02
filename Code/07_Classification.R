e8139242691a0b3276b4c2adce2caa3012831b3c
Jomax1 <maxime1.joly@gmail.com>
2025-04-02 16:28:22 +0200

    Update 07_Classification.R

--- Code/07_Classification.R
+++ Code/07_Classification.R
@@ -1,1 +1,12 @@
 # Code for classifying images
+
+library(terra)
+library(imageRy)
+
+im.list()
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 = flip(mato1992)
+plot(mato1992)
+
+
+
