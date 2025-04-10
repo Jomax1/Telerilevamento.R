9d4c8e26e06a0376e868a186f612da50bcbd664d
Jomax1 <maxime1.joly@gmail.com>
2025-04-10 15:50:17 +0200

    Create 09_packages_from_Github.R

---
+++ Code/09_packages_from_Github.R
@@ -0,0 +1,15 @@
+# R code for installing pasckages
+
+# from github
+install_github("ducciorocchini/cblindplot") # color blind plot
+library(cblindplot)
+
+install_github("clauswilke/colorblindr") # di Claus Wilke
+library(colorblindr)
+
+# from CRAN
+install.packages("devtools")
+library(devtools) # or remotes
+
+install.packages("colorblindcheck")
+library(colorblindcheck)
