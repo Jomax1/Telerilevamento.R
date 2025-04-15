a362624ca9667eaeba6d86d3a7b70c6193c51f44
Jomax1 <maxime1.joly@gmail.com>
2025-04-15 17:03:23 +0200

    Update 10_pca.R

--- Code/10_pca.R
+++ Code/10_pca.R
@@ -3,4 +3,27 @@
 library(terra)
 library(imageRy)
 
+sent = im.import("sentinel.png")
+sent = flip(sent)
+plot(sent)
+
+sent = c(sent[[1]], sent[[2]], sent[[3]])
+plot(sent)
+     
+# NIR = band 1
+# red = band 2
+# green = band 3
+
+sentpca = im.pca(sent, n_samples = 100000)
+
+tot = 78 + 54 + 6 # 138
+
+# 78 : 138 = x : 100
+78 * 100 / tot
+
+sdpc1 = focal(sentpca[[1]], w = c(3,3), fun="sd")
+plot(sdpc1)
+
+
+
 
