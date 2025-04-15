b3adb29e2691762f2019ce2a3e2b40808638b078
Jomax1 <maxime1.joly@gmail.com>
2025-04-15 17:40:41 +0200

    Update 11_functions.R

--- Code/11_functions.R
+++ Code/11_functions.R
@@ -5,4 +5,34 @@ somma <- function(x,y) {
   return(z)
   }
 
+differenza <- function(x,y) {
+  z = x - y
+  return(z)
+  }
+
+# multiframe
+mf <- function(nrow,ncol) {
+  par(mfrow=c(nrow,ncol))
+  }
+
+positivo <- function(x) 
+  {
+  if(x>0)
+    {
+    print("Questo numero è positivo, non lo sai?")
+    }
+  else if(x<0)
+    {
+    print("Questo numero è negativo")
+    }
+  else 
+    {
+    print("Lo zéro è zero")
+    }
+  }
+
+  
+
+
+
 
